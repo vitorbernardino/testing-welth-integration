2406e3b98a01333d542d62cdca94141fd74b0149
¨Vitor <vitor.hernandorena@hotmail.com>
2025-09-30 13:41:38 -0300

    fix: changing port to render compatibility

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api/v1');
 
-  const port = process.env.PORT || 3001;
+  const port = process.env.PORT || 10000;
   await app.listen(port);
   console.log(`🚀 Welth API running on port ${port}`);
 }
